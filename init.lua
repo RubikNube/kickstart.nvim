d07b027d69c6f763f003f720e6553267f4ac2ad5
RubikNube <roland@kiesendahl.de>
2025-07-20 22:58:55 +0200

    keymap for searching references with telescope

--- init.lua
+++ init.lua
@@ -539,10 +539,11 @@ require('lazy').setup({
             vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
             vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>se', builtin.resume, { desc = '[S]earch [E]xisting Results' })
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
             vim.keymap.set('n', '<leader>sv', builtin.lsp_document_symbols, { desc = '[S]earch [V]iew Document Symbols' })
+            vim.keymap.set('n', '<leader>sr', builtin.lsp_references, { desc = '[S]earch [R]eferences' })
 
             -- Slightly advanced example of overriding default behavior and theme
             vim.keymap.set('n', '<leader>/', function()
