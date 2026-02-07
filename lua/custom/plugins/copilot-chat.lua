dab1edba0a46709cf886e7d8d1be4bbb20960ea9
RubikNube <roland@kiesendahl.de>
2026-02-07 16:26:07 +0100

    feat(copilot-chat): add model and keymaps
    Set CopilotChat to use the gpt-5.2 model by default. Add keymaps for
    model selection and additional actions (commit/fix/optimize/prompt)
    alongside the existing toggle and explain mappings.

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -6,11 +6,13 @@ return {
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
     opts = {
-        -- See Configuration section for options
+        model = 'gpt-5.2',
     },
     vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatToggle<CR>', { desc = '[C]opilot [C]hat Toggle' }),
+    vim.keymap.set('n', '<leader>cm', '<cmd>CopilotChatModel<CR>', { desc = '[C]opilot [M]odel' }),
+    vim.keymap.set('n', '<leader>cg', '<cmd>CopilotChatCommit<CR>', { desc = '[C]opilot [G]it Commit' }),
+    vim.keymap.set('v', '<leader>cf', '<cmd>CopilotChatFix<CR>', { desc = '[C]opilot [F]ix' }),
     vim.keymap.set('v', '<leader>ce', ':CopilotChatExplain<CR>', { desc = '[C]opilot [E]xplain (visual)' }),
-    vim.keymap.set('n', '<leader>cq', '<cmd>CopilotChatQuick<CR>', { desc = '[C]opilot [Q]uick Chat' }),
-    vim.keymap.set('n', '<leader>c?', '<cmd>CopilotChat<CR>', { desc = '[C]opilot [C]hat' }),
-    vim.keymap.set('n', '<leader>cf', '<cmd>CopilotChatFix<CR>', { desc = '[C]opilot [F]ix' }),
+    vim.keymap.set('v', '<leader>co', '<cmd>CopilotChatOptimize<CR>', { desc = '[C]opilot [O]ptimize' }),
+    vim.keymap.set('v', '<leader>cp', '<cmd>CopilotChatPrompt<CR>', { desc = '[C]opilot [P]rompt' }),
 }
