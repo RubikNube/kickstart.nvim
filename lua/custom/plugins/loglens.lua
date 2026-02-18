ef1872f6ef7b03f0f79be67d09187f10cb701279
RubikNube <roland@kiesendahl.de>
2026-02-19 00:16:45 +0100

    feat(loglens): add loglens.nvim plugin with patterns and keys
    Add loglens.nvim plugin configuration with sensible log patterns for ERROR,
    WARN, INFO, DEBUG, and timeout. Set up keybindings for LogLens commands to
    open, close, configure, edit, load, and save log pattern configurations.

---
+++ lua/custom/plugins/loglens.lua
@@ -0,0 +1,22 @@
+return {
+    'RubikNube/loglens.nvim',
+    event = 'BufRead',
+    config = function()
+        require('loglens').setup {
+            patterns = {
+                { regex = 'ERROR', fg = '#ffffff', bg = '#ff0000' },
+                { regex = 'WARN', fg = '#000000', bg = '#ffff00' },
+                { regex = 'INFO', fg = '#ffffff', bg = '#0000ff' },
+                { regex = 'DEBUG', fg = '#000000', bg = '#00ffff' },
+                { regex = 'timeout', fg = '#ffffff', bg = '#ff8800' },
+            },
+        }
+        -- Keybindings for LogLens commands
+        vim.keymap.set('n', '<leader>lo', ':LogLensOpen<CR>', { desc = 'LogLens: Open analysis window' })
+        vim.keymap.set('n', '<leader>lc', ':LogLensClose<CR>', { desc = 'LogLens: Close window' })
+        vim.keymap.set('n', '<leader>lC', ':LogLensConfigure<Space>', { desc = 'LogLens: Configure pattern' })
+        vim.keymap.set('n', '<leader>le', ':LogLensConfigureOpen<CR>', { desc = 'LogLens: Edit patterns (JSON)' })
+        vim.keymap.set('n', '<leader>ll', ':LogLensLoad<CR>', { desc = 'LogLens: Load pattern config' })
+        vim.keymap.set('n', '<leader>ls', ':LogLensSave<CR>', { desc = 'LogLens: Save pattern config' })
+    end,
+}
